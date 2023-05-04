92deb771a8494ad91b7c1dd6cede1c622a235bef
hugues <ghugues@netc.fr>
2023-05-04 22:48:03 +0200

    Detect portal URL and rework code base

--- wifirst.go
+++ wifirst.go
@@ -4,37 +4,49 @@ import (
 	"bytes"
 	"encoding/json"
 	"flag"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"net/url"
+	"os"
 )
 
-type SessionsCode struct {
-	Radius struct {
-		Login    string `json:"login"`
-		Password string `json:"password"`
-	} `json:"radius"`
-}
-
 func main() {
-	l := flag.String("l", "", "The login")
-	p := flag.String("p", "", "The password")
+	flagLogin := flag.String("l", "", "The login")
+	flagPassword := flag.String("p", "", "The password")
 	flag.Parse()
-	if *l == "" || *p == "" {
+
+	if *flagLogin == "" || *flagPassword == "" {
 		flag.Usage()
-		return
+		os.Exit(1)
 	}
 
-	boxToken := getBoxToken()
-	sessionCode := getSessionCode(boxToken, *l, *p)
-	sendToken(sessionCode)
+	portal := getPortal()
+	boxToken := getBoxToken(portal)
+	login, password := getSessionCode(portal, boxToken, *flagLogin, *flagPassword)
+	sendToken(portal, login, password)
 }
 
-func getBoxToken() string {
-	const url = "https://portal-front.wifirst.net/"
+func getPortal() *url.URL {
+	client := http.Client{CheckRedirect: func(*http.Request, []*http.Request) error { return fmt.Errorf("One request") }}
+	response, err := client.Get("http://detectportal.firefox.com/sucess.txt")
+	if response == nil {
+		log.Fatal(err)
+	}
+	switch portal, err := response.Location(); err {
+	case nil:
+		return portal
+	case http.ErrNoLocation:
+		log.Fatal("No location are present in the response header, you should be already connected.")
+	default:
+		log.Fatal("Error when get location response header:", err)
+	}
+	return nil // never executed because the previous log.Fatal call.
+}
 
-	log.Println("GET", url)
-	boxTokenResponse, err := http.Get(url)
+func getBoxToken(portal *url.URL) string {
+	boxTokenResponse, err := http.Get(portal.String())
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -48,48 +60,82 @@ func getBoxToken() string {
 	return string(boxToken)
 }
 
-func getSessionCode(boxToken, login, password string) (sessionsCode SessionsCode) {
+func getSessionCode(portal *url.URL, boxToken, email, inputPassword string) (login, password string) {
 	data, _ := json.Marshal(map[string]string{
-		"email":       login,
-		"password":    password,
-		"box_token":   string(boxToken),
+		"email":       email,
+		"password":    inputPassword,
+		"box_token":   boxToken,
 		"fragment_id": "12514",
 	})
 
 	sessionsResponse := post(
-		"https://portal-front.wifirst.net/api/sessions",
+		replacePath(portal, "/api/sessions"),
 		"application/json",
 		data,
 	)
-	defer sessionsResponse.Body.Close()
 
-	err := json.NewDecoder(sessionsResponse.Body).Decode(&sessionsCode)
-	if err != nil {
+	sessionsCode := struct {
+		Radius struct {
+			Login    string `json:"login"`
+			Password string `json:"password"`
+		} `json:"radius"`
+	}{}
+
+	if err := json.Unmarshal(sessionsResponse, &sessionsCode); err != nil {
 		log.Fatal(err)
 	}
 
-	return
+	return sessionsCode.Radius.Login, sessionsCode.Radius.Password
 }
 
-func sendToken(sessionsCode SessionsCode) {
-	loginResponse := post(
+func sendToken(portal *url.URL, login, password string) {
+	data := url.Values{
+		"username":       {login},
+		"password":       {password},
+		"success_url":    {"https://www.google.fr/"},
+		"error_url":      {replacePath(portal, "/connect-error")},
+		"update_session": {"0"},
+	}
+
+	post(
 		"https://wireless.wifirst.net/goform/HtmlLoginRequest",
 		"application/x-www-form-urlencoded",
-		[]byte("username="+sessionsCode.Radius.Login+
-			"&password="+sessionsCode.Radius.Password+
-			"&success_url=https%3A%2F%2Fwww.google.fr%2F&error_url=https%3A%2F%2Fportal-front.wifirst.net%2Fconnect-error&update_session=0"),
+		[]byte(data.Encode()),
 	)
-
-	defer loginResponse.Body.Close()
-	io.Copy(io.Discard, loginResponse.Body)
 }
 
-// Make a POST HTTP request
-func post(url, contentType string, data []byte) *http.Response {
-	log.Println("POST", url)
-	response, err := http.Post(url, contentType, bytes.NewReader(data))
+func post(u, contentType string, data []byte) []byte {
+	response, err := http.Post(u, contentType, bytes.NewReader(data))
 	if err != nil {
 		log.Fatal(err)
 	}
-	return response
+
+	defer response.Body.Close()
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return body
+}
+
+/* TOOLS FUNCTION */
+
+// Create a new URL by replacing the path and output the result as string
+func replacePath(u *url.URL, path string) string {
+	return u.ResolveReference(&url.URL{Path: path}).String()
+}
+
+// Log the request method and URL, then make the request with wraped http round tripper.
+type LogRoundTripper struct {
+	http.RoundTripper
+}
+
+func (l LogRoundTripper) RoundTrip(request *http.Request) (*http.Response, error) {
+	log.Println(request.Method, request.URL)
+	return l.RoundTripper.RoundTrip(request)
+}
+
+func init() {
+	http.DefaultTransport = LogRoundTripper{http.DefaultTransport}
 }
